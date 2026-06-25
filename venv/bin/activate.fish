9c37b6cf3bbc5b9bacae621dd9e23f42b5e880c0
Afzal Khan <afzalkhan01765@gmail.com>
2026-06-25 22:30:33 +0600

    updated code

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/betopi/Desktop/afzalkhan101-portfolio/venv
+set -gx VIRTUAL_ENV /home/hd/Desktop/afzalkhan101-portfolio/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
